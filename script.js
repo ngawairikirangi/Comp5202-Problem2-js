7778152f263336ccacdea49d82ae5344f0de9eb9
Ngawai Rikirangi <9978212@BCS.NET.NZ>
2017-09-04 11:24:16 +1200

    problem2-js

---
+++ script.js
@@ -0,0 +1,13 @@
+let division =prompt("Please enter a number less than 100", "00")
+
+
+for (let 1=0; i<12) i++; {
+    division = parseInt(division)
+
+    let a = i + 1
+
+    Console.Log(i + "/" + division + "=" + (i / division))
+
+    
+    
+}
